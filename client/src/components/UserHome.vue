90395b457f10d439468b7af779eef609738c8006
Ashik <ajashik@outlook.com>
2017-11-25 16:17:34 +0530

    Font cdn Added

--- client/src/components/UserHome.vue
+++ client/src/components/UserHome.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-
+    <nav>
+      <navbar></navbar>
+    </nav>
   </div>
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar'
+
   export default {
+    name: 'UserHome',
+    components: {
+      'navbar': Navbar
+    }
   }
 </script>
 
