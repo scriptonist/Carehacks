27fc734b4f5073030611ede7fd386444ea4ca9d6
ajomadlabs <ajojohn555@gmail.com>
2017-11-25 23:55:20 +0530

    Frontend: Refactored Code

--- client/src/components/PharmaStore.vue
+++ client/src/components/PharmaStore.vue
@@ -5,6 +5,13 @@
       <m-navbar v-bind:navlist="navlist">></m-navbar>
     </nav>
 
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
